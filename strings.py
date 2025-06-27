9fd77e77d84b266e39a6c103c4e4fee7f19a6230
T213-web <tiaan.oosthuizen420@gmail.com>
2025-06-27 17:51:53 +0200

    Update strings.py

--- strings.py
+++ strings.py
@@ -4,7 +4,7 @@ message = " Hello, world "
 
 print(message.strip())  # Removes leading and trailing whitespace
 print(message.lower())  # Converts to lowercase
-print(message.split(', '))  # Splits the string into a list at each commas
+print(message.split(', '))  # Splits the string into a list at each comma
 
 print(message.upper())  # Converts to uppercase
 print(message.replace("world", "Python"))  # Replaces 'world' with 'Python'
