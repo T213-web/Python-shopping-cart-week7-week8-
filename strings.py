525c3eae8c8245f6f3e5d3facfa911bfc9931301
T213-web <tiaan.oosthuizen420@gmail.com>
2025-06-27 17:39:59 +0200

    Initial commit

---
+++ strings.py
@@ -0,0 +1,10 @@
+#strings
+
+message = " Hello, world "
+
+print(message.strip())  # Removes leading and trailing whitespace
+print(message.lower())  # Converts to lowercase
+print(message.split(', '))  # Splits the string into a list at each comma
+
+print(message.upper())  # Converts to uppercase
+print(message.replace("world", "Python"))  # Replaces 'world' with 'Python'
